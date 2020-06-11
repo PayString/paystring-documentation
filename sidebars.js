1207dbef65a20ea2003d9c7c3605222daa2ea580
Lois Patterson <lpatterson@ripple.com>
2020-06-11 16:59:58 -0700

    Add Integrate User Database into PayID

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    PayID: ['payid-overview','getting-started', 'payid-workflow','xpring-sdk-payid','api-reference', 'metrics', 'payid-best-practices'],
+    PayID: ['payid-overview','getting-started', 'payid-workflow', 'integrate-payid-users', 'xpring-sdk-payid','api-reference', 'metrics', 'payid-best-practices'],
     Deployment: ['remote-deployment', 'local-deployment'],
   },
 }
