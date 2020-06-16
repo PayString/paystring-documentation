65263ec883968147cc97d56a0474f3f3901edcda
Hans Bergren <hansbergren@gmail.com>
2020-06-16 10:21:39 -0400

    Add PayID Server Requirements (#21)
    Add the recommended requirements for running a PayID server.

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,19 @@
 module.exports = {
   someSidebar: {
-    PayID: ['payid-overview','getting-started', 'payid-workflow', 'integrate-payid-users', 'xpring-sdk-payid','api-reference', 'metrics', 'payid-best-practices'],
-    Deployment: ['remote-deployment', 'local-deployment'],
+    PayID: [
+      'payid-overview',
+      'getting-started',
+      'payid-workflow',
+      'integrate-payid-users',
+      'xpring-sdk-payid',
+      'api-reference',
+      'metrics',
+      'payid-best-practices',
+    ],
+    Deployment: [
+      'payid-server-requirements',
+      'remote-deployment',
+      'local-deployment',
+    ],
   },
 }
