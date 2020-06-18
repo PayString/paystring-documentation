0aeb107ea0b61bdf9e0a27c20176942b2481fa13
Tyler Storm <stormtv@mail.uc.edu>
2020-06-17 21:00:13 -0400

    fix sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,24 +1,43 @@
 module.exports = {
-  sidebar: {
-    'Getting Started': ['getting-started'],
-    'PayID Protocol': [
-      'payid-overview',
-      'payid-headers',
-      'payid-interfaces',
-      'payid-schemas',
-    ],
-    'PayID Reference Implementation': [
-      'payid-reference-overview',
-      'api-reference',
-      'payid-best-practices',
-      'integrate-payid-users',
-      'metrics',
-    ],
-    Deployment: [
-      'payid-server-requirements',
-      'remote-deployment',
-      'local-deployment',
-    ],
-    'PayID Tooling': ['xpring-sdk-payid'],
-  },
+  docs: [
+    {
+      type: 'doc',
+      id: 'getting-started',
+    },
+    {
+      type: 'category',
+      label: 'PayID Protocol',
+      items: [
+        'payid-overview',
+        'payid-headers',
+        'payid-interfaces',
+        'payid-schemas',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'PayID Reference Implementation',
+      items: [
+        'payid-reference-overview',
+        'api-reference',
+        'payid-best-practices',
+        'integrate-payid-users',
+        'metrics',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Deployment',
+      items: [
+        'payid-server-requirements',
+        'remote-deployment',
+        'local-deployment',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'PayID Tooling',
+      items: ['xpring-sdk-payid'],
+    },
+  ],
 }
