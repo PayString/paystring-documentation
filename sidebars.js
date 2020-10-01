3da5b5ecbaa1631d01de0663f717c37f64fb9774
Lois Patterson <lpatterson@ripple.com>
2020-10-01 11:13:45 -0700

    fixed sidebar

--- sidebars.js
+++ sidebars.js
@@ -46,7 +46,6 @@ module.exports = {
       items: [
         'api-reference',
         'payid-cli-reference',
-        'payid-discovery',
         'payid-headers',
         'payid-interfaces',
         'payid-schemas',
