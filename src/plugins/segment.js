f865801b19e518a1f70b4e5ad464b8228d062994
Tyler Storm <stormtv@mail.uc.edu>
2020-10-15 14:31:20 -0400

    don't run locally

--- src/plugins/segment.js
+++ src/plugins/segment.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 const DEFAULT_OPTIONS = {
   appId: null,
-  enableOnDevMode: true, // if 'false', segment will be fired on NODE_ENV=production only
+  enableOnDevMode: false, // if 'false', segment will be fired on NODE_ENV=production only
 }
 
 module.exports = function (context, opts) {
